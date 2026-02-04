69f575f5ed7257361b5e8fdbc5edf089d73172b4
AirCraft009 <coconraupe@gmail.com>
2026-02-04 20:39:57 +0100

    updated constants.go to remove name mangling

--- internal/helper/constants.go
+++ internal/helper/constants.go
@@ -1,14 +1,14 @@
 package helper
 
 const (
-	MemorySize           = 1024 * 16
-	ProgramStdLibStart   = 0x1800
-	InterrupttableLoc    = 23965
-	IncludeSignifier     = "#include"
-	StdLibLocation       = "stdlib"
-	StdlibLocationUse    = "lib/stdlib"
-	includeLocationUse   = "lib/include"
-	GlobalHeaderLocation = "/lib/globalHeaders"
-	StdLibLocationUse    = StdlibLocationUse + "/obj"
-	IncludeLocationUse   = includeLocationUse + "/obj"
+	MemorySize              = 1024 * 16
+	ProgramStdLibStart      = 0x1800
+	InterrupttableLoc       = 23965
+	IncludeSignifier        = "#include"
+	StdLibLocationSignifier = "stdlib"
+	StdlibLocation          = "lib/stdlib"
+	includeLocation         = "lib/include"
+	GlobalHeaderLocation    = "/lib/globalHeaders"
+	StdLibLocationUse       = StdlibLocation + "/obj"
+	IncludeLocationUse      = includeLocation + "/obj"
 )
