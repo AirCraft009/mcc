ade9b8f081f25c83400dbd40d93d56817c8fa901
AirCraft009 <coconraupe@gmail.com>
2026-02-07 02:08:06 +0100

    rebuilt stdlib

--- lib/include/sources/interruptTable.asm
+++ lib/include/sources/interruptTable.asm
@@ -1,9 +1,7 @@
-
 _interruptTable:
     JMP _interrupt          # handles saving the current task and goes back to the right offset
     JMP _keyboard_handler   # always jump so that the next RET call goes back to the scheduler
     JMP _timer_handler
-
 
 _keyboard_handler:
     MOVI T2 2
